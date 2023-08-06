bde3f60390bcaf756d6553cd5fd83faa7ab8b933
Sparks-Lu <lostinfog@126.com>
2023-08-06 12:20:43 +0800

    Updated scripts.

---
+++ install_mmagic.sh
@@ -0,0 +1,5 @@
+pip install torch
+pip install openmim
+mim install 'mmcv>=2.0.0'
+mim install 'mmengine'
+mim install 'mmagic'
---
+++ install_nodejs_14.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node -v
+sudo apt-get install -y libtool pkg-config autoconf nasm python2
