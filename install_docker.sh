519793c5ba81e83ea0708be994bb376862f67899
Sparks-Lu <lostinfog@126.com>
2023-08-22 05:49:56 +0800

    Updated docker install script.

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install -y docker-ce=5:20.10.22~3-0~ubuntu-bionic docker-ce-cli=5:20.10.22~3-0~ubuntu-bionic containerd.io docker-compose-plugin=2.14.1~ubuntu-bionic
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # install nvidia container toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
