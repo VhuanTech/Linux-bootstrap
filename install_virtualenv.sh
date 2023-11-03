7e4f95dacfb9c66b12aa273c602d252789fd8f51
Sparks-Lu <lostinfog@126.com>
2023-11-03 16:39:26 +0800

    Updated virtualenv installer.

--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -3,7 +3,7 @@ sudo apt install -y virtualenv virtualenvwrapper
 sudo python3 -m pip install setuptools
 sudo python3 -m pip install virtualenvwrapper --trusted-host mirrors.aliyun.com
 cat << EOF >> ~/.bashrc
-export WORKON_HOME=/data/virtualenvs
+export WORKON_HOME=/data/system/virtualenvs
 export PROJECT_HOME=/data/workspace
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
