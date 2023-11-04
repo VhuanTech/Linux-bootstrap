8931d508547ec6a80aa2f387f22bea001d18826e
Sparks-Lu <lostinfog@126.com>
2023-11-04 14:10:10 +0800

    Updated python3.7 install script.

--- install_py3.7.sh
+++ install_py3.7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7
+sudo apt install -y python3.7 python3.7-distutils
